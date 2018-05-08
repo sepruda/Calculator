91d0825614028bc921a0a2586f2793d80d3d83f3
Christian Jess Mark <christianjessmark@gmail.com>
2018-05-08 22:38:48 +0200

    Functioning 100%
    Needs styling and maybe refactoring

--- js/script.js
+++ js/script.js
@@ -3,48 +3,44 @@ const keys = document.querySelector('.calculator-keys');
 const display = document.querySelector('.calculator-display');
 
 keys.addEventListener('click', e => { //event listener on click taking argument 'e'
-	//pure functions
-	const resultString = createResultString
-
-	const createResultString = () => {
-			if (!action) {
-				return displayedNum === '0' || //If displayed number is 0 replace it with number pressed
-				previousKeyType === 'operator' || //or the user pressed an operator and then a number
-				previousKeyType === 'calculate' //or if the user just pressed the equel button
-				? keyContent // return the key pressed
-				: displayedNum + keyContent //otherwise append number to number in display
-				}
-					calculator.dataset.previousKeyType = 'action';
-		}
-		// conditions for decimal button
-		if (action === 'decimal') {
-			if (!displayedNum.includes('.')) return = displayedNum + '.';
-			if (previousKeyType === 'operator' && !displayedNum.includes('.') &&
-				previousKeyType === 'calculate') return = '0.';
-			return displayedNum;
-			} 
-			calculator.dataset.previousKeyType = 'decimal';
-		} 
-
-	//impure stuff
-	display.textContent = resultString
-	updateCalculatorState
 	if (e.target.matches('button')) {  //if e is a button tag
-		const key = e.target; //key pressed 
-		const action = key.dataset.action; //data-action key has
-		const keyContent = key.textContent; //What the key displays
-		const displayedNum = display.textContent; //displayed number
-		const previousKeyType = calculator.dataset.previousKeyType //saving what key was just pressed
+	const key = e.target; //key pressed 
+	const action = key.dataset.action; //data-action key has
+	const keyContent = key.textContent; //What the key displays
+	const displayedNum = display.textContent; //displayed number
+	const previousKeyType = calculator.dataset.previousKeyType //saving what key was just pressed
 
-		//calculate function			
-		const calculate = (n1, operator, n2) => {
-			const firstNum = parseFloat(n1);
-			const secondNum = parseFloat(n2);
-			if (operator === 'add')	return firstNum + secondNum;
-			if (operator === 'subtract') return firstNum - secondNum;
-			if (operator === 'multiply') return firstNum * secondNum;
-			if (operator === 'divide') return firstNum / secondNum;
+	if (!action) {
+		if (displayedNum === '0'|| //If displayed number is 0 replace it with number pressed
+		previousKeyType === 'operator' || //or the user pressed an operator and then a number
+		previousKeyType === 'calculate') {  //or if the user just pressed the equel button
+			display.textContent = keyContent; // return the key pressed
+		} else {
+			display.textContent = displayedNum + keyContent; //otherwise append number to number in display
 		}
+		calculator.dataset.previousKeyType = 'action';
+	}
+	// conditions for decimal button
+	if (action === 'decimal') {
+		if (!displayedNum.includes('.')) {
+			display.textContent = displayedNum + '.';
+		} else if 
+		(previousKeyType === 'operator' && !displayedNum.includes('.') ||
+			previousKeyType === 'calculate') {
+			display.textContent = '0.';
+		}
+		calculator.dataset.previousKeyType = 'decimal';
+		}  
+
+	//calculate function			
+	const calculate = (n1, operator, n2) => {
+		const firstNum = parseFloat(n1);
+		const secondNum = parseFloat(n2);
+		if (operator === 'add')	return firstNum + secondNum;
+		if (operator === 'subtract') return firstNum - secondNum;
+		if (operator === 'multiply') return firstNum * secondNum;
+		if (operator === 'divide') return firstNum / secondNum;
+	}
 		
 		//conditions for operator buttons
 		if ( 
@@ -107,6 +103,5 @@ keys.addEventListener('click', e => { //event listener on click taking argument 
 		calculator.dataset.modValue = secondValue;
 		calculator.dataset.previousKeyType = 'calculate';
 		}
-
 	}
 });
